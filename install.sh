34c7c609fbb608c2402fab7eaa99f62ab8a7dace
Paul Dowman <paul@pauldowman.com>
2021-08-04 13:41:36 -0600

    Change zsh theme to powerlevel10k

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #/bin/bash
 
+# install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# install powerleve10k zsh theme
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 for FILE in `ls ~/dotfiles` ; do 
   ln -sf ~/dotfiles/$FILE ~/.$FILE
