5eed2647dbd6e72a586d0b22ee3376ac3aa7f580
Paul Dowman <paul@pauldowman.com>
2022-01-22 13:27:01 -0700

    Fix for any dotfile dir location

--- install.sh
+++ install.sh
@@ -1,11 +1,15 @@
 #/bin/bash
 
+DOTFILEDIR=`dirname $0`
+
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # install powerleve10k zsh theme
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-for FILE in `ls ~/dotfiles` ; do 
-  ln -sf ~/dotfiles/$FILE ~/.$FILE
+for FILE in `ls $DOTFILEDIR` ; do 
+  echo $FILE
+  rm ~/.$FILE
+  ln -s $DOTFILEDIR/$FILE ~/.$FILE
 done
