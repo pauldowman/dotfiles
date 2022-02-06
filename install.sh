f12edbf82a66f469a4827aeff55216e53ef5928e
Paul Dowman <paul@pauldowman.com>
2022-02-06 15:44:12 -0700

    Remove powerlevel10k

--- install.sh
+++ install.sh
@@ -5,9 +5,6 @@ DOTFILEDIR=`dirname $0`
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# install powerleve10k zsh theme
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
 for FILE in `ls $DOTFILEDIR` ; do 
   echo $FILE
   rm ~/.$FILE
